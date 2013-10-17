56f7e3060fd579c72dd6db6598df5cb4228c0e00
mubaiedj <mubaied.jm@gmail.com>
2013-10-17 13:09:46 +0200

    Creacion del metodo primo

--- src/spai/egit2/NumeroEntero.java
+++ src/spai/egit2/NumeroEntero.java
@@ -1,38 +1,45 @@
 package spai.egit2;
 
 public class NumeroEntero {
-    private int valor;
+	private int valor;
 
-    public NumeroEntero(int valor) {
-        this.valor = valor;
-    }
+	public NumeroEntero(int valor) {
+		this.valor = valor;
+	}
 
-    public NumeroEntero() {
-        this(0);
-    }
+	public NumeroEntero() {
+		this(0);
+	}
 
-    public int getValor() {
-        return valor;
-    }
+	public int getValor() {
+		return valor;
+	}
 
-    private void setValor(int valor) {
-        this.valor = valor;
-    }
+	private void setValor(int valor) {
+		this.valor = valor;
+	}
 
-    public void incrementar() {
-        this.setValor(this.valor + 1);
-    }
+	public void incrementar() {
+		this.setValor(this.valor + 1);
+	}
 
-    public void doblar() {
-        this.setValor(this.valor * 2);
-    }
+	public void doblar() {
+		this.setValor(this.valor * 2);
+	}
 
-    public void decrementar() {
-        this.valor--;
-    }
+	public void decrementar() {
+		this.valor--;
+	}
 
 	public boolean isPrimo(NumeroEntero numero) {
-		return false;
+		int contador = 2;
+		boolean primo = true;
+		while ((primo) && (contador != numero.getValor())) {
+			if (numero.getValor() % contador == 0)
+				primo = false;
+			contador++;
+		}
+		return primo;
 	}
 
 }
